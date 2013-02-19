46b18475fcc1ae488ebd8c6e93d2f60328de467c
Fernando Campo <fernando.campo@o2.com>
2013-02-19 13:36:50 +0000

    Added inputs

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 document.getElementById('reload-1').onclick = function () {
-  document.getElementById('iframe-1').src = document.getElementById('iframe-1').src;
+  document.getElementById('iframe-1').src = document.getElementById('input-1').value ||
+                                            document.getElementById('iframe-1').src;
 };
 
 document.getElementById('reload-2').onclick = function () {
-  document.getElementById('iframe-2').src = document.getElementById('iframe-2').src;
+  document.getElementById('iframe-2').src = document.getElementById('input-2').value ||
+                                            document.getElementById('iframe-2').src;
 };
 
 document.getElementById('reload-3').onclick = function () {
-  document.getElementById('iframe-3').src = document.getElementById('iframe-3').src;
+  document.getElementById('iframe-3').src = document.getElementById('input-3').value ||
+                                            document.getElementById('iframe-3').src;
 };
 
 document.getElementById('reload-4').onclick = function () {
-  document.getElementById('iframe-4').src = document.getElementById('iframe-4').src;
+  document.getElementById('iframe-4').src = document.getElementById('input-4').value ||
+                                            document.getElementById('iframe-4').src;
 };
 
 
