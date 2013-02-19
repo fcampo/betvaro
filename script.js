06962e359098168bc58d2e916fdf5bd958253a15
Fernando Campo <fernando.campo@o2.com>
2013-02-19 12:06:59 +0000

    Initial

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('reload-1').onclick = function () {
+  document.getElementById('iframe-1').src = document.getElementById('iframe-1').src;
+};
+
+document.getElementById('reload-2').onclick = function () {
+  document.getElementById('iframe-2').src = document.getElementById('iframe-2').src;
+};
+
+document.getElementById('reload-3').onclick = function () {
+  document.getElementById('iframe-3').src = document.getElementById('iframe-3').src;
+};
+
+document.getElementById('reload-4').onclick = function () {
+  document.getElementById('iframe-4').src = document.getElementById('iframe-4').src;
+};
+
+
